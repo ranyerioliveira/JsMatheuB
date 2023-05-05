8a6ccb2d180dd8f4d2960f1c5940c5e4c6b8ae0d
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-05 19:44:39 -0300

    JS
    Variaveis

---
+++ 3_particularidadesJs/script.js
@@ -0,0 +1,17 @@
+console.log('Primeiro');
+
+console.log('Último');
+
+var a = 1
+
+a = 'teste'
+
+console.log(a);
+
+//Esse comonetario é de unica linhas
+
+/*
+esse é um comentario de multiplas linhas
+*/
+
+//Cuidado com comentarios, eles são visiveis no codigo no navegador
---
+++ 4_variaveis/script.js
@@ -0,0 +1,34 @@
+var teste = 1;
+
+
+console.log(teste);
+
+teste ="Ranyeri";
+
+var nome = "João"
+
+console.log(teste);
+console.log(nome);
+
+var $nome = 'Felipe';
+var _nome = 'Chico';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'José';
+console.log(nome5);
+
+var meuPrimeiroNome = "cunha";
+console.log(meuPrimeiroNome);
+
+let testando = 1
+const ola = 2
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+meuNome = 45
+
+console.log(meuNome);
