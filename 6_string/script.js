525c26f67bb07a7e3a2e663d5448379ea2bdf5db
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-05 21:41:45 -0300

    Boolean
    Boolean

---
+++ 6_string/script.js
@@ -0,0 +1,15 @@
+console.log("ola" + " eu sou um Humano");
+
+var $nome = 'Ranyeri';
+var $sobreNome = 'Cunha';
+
+console.log($nome);
+console.log($sobreNome);
+
+console.log($nome + ' ' + $sobreNome);
+
+var frase = 'Meu nome completo é';
+
+console.log(frase + ' ' + $nome + ' ' + $sobreNome)
+
+document.write(frase + ' ' + $nome + ' ' + $sobreNome);
---
+++ 7_boolean/script.js
@@ -0,0 +1,18 @@
+var nun = 2;
+var nun1 = 3;
+
+console.log(2>3);
+
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+console.log(falso);
+console.log(typeof falso);
+
+document.write("<p style = 'color: red'>As variáveis não podem ser nomeadas com 'False e true' ou até mesmo com o 'function', essas 'palavras' estão reservadas para funcionalidades dentro do JavaScript</p>")
