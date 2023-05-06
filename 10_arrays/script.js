116a931f56643cab687f0b9d2204e1e0fd9794f8
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-06 16:50:28 -0300

    Arrays
    Arrays

--- 10_arrays/script.js
+++ 10_arrays/script.js
@@ -1,4 +1,20 @@
+document.write("Array = [], objetos {}, uma array aceita qualquer tipo de dados, até mesmo um objeto");
+
+var teste = ['Ranyeri', 'João', 'Marcio', 0 ];
+
+console.log(teste)
+console.log(typeof teste);
 
 
+document.write("<br>Para acessar algum elemento da uma array se chama a array declara e em saguida o item em []");
 
+console.log(teste[2]);
+console.log(teste[0]);
 
+//abaixo como insserir um indici novo, sem o var.
+
+teste[4] = "Natal";
+console.log(teste);
+
+teste[1] = "Karen";
+console.log(teste);
