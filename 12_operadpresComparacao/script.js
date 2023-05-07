d20ebe1561b4897fecc4af567aa2f0ecbb760b10
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-07 16:00:19 -0300

    Operadores
    Operadores

---
+++ 12_operadpresComparacao/script.js
@@ -0,0 +1,34 @@
+var idade  = 18;
+var possuiCarro = 1;
+
+if(idade >= 18){
+    console.log("O Usuario pode retirar a carteira de habilitação")
+}
+
+if(idade <= 17){
+    console.log("Não te idade para retirar habilitação")
+};
+
+if(possuiCarro){
+    console.log("O usuario pode andar de carro")
+};
+
+var nome = "Ranyeri";
+
+if(nome == "Ranyeri"){
+    console.log("Olá Ranyeri")
+};
+
+if(nome != "Ranyeri"){
+    console.log("Você não é Ranyeri")
+}
+
+var valor = 11;
+
+if(valor > 10){
+    console.log("O numero é MAIOR que o valor desejado")
+}
+if(valor < 10){
+    console.log("O numero é MENOR que o valor")
+}
+
