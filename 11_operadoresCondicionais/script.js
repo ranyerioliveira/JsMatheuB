36757b8aa735931c8919bfdb0a13a60acda780bf
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-07 11:46:55 -0300

    If
    Construindo peradores com if

---
+++ 11_operadoresCondicionais/script.js
@@ -0,0 +1,16 @@
+
+console.log("Antes do if");
+
+if(true){
+    console.log("Testando de if")
+};
+
+console.log("Depois do If");
+
+var idade = 19;
+
+if(idade > 18){
+    console.log("Você pode dirigir")
+}
+
+if
