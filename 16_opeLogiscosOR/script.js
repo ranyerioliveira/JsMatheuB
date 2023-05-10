f7812a4d319d64fc2c5637929f6939fbe9ca617d
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-10 08:17:35 -0300

    Operadores OR ||
    Estudo de operadores OR ||

---
+++ 16_opeLogiscosOR/script.js
@@ -0,0 +1,38 @@
+
+
+document.write("<h2>OR ||</h2><p>Caso uma das expresões der true e a outra der falso a função sera executada se tiver o || or</p>");
+
+document.write("<p>Quando uma das funções dar true e a outra da false, resultado que a função é execultada</p>");
+
+/*var idade = 19;
+var nome = "João";
+
+if(nome =="Carlos" || idade >= 18){
+    console.log("Ja pode ir para a balada")
+}else{
+    console.log("Não pode entrar")//caso as expressões sejam false
+};*/
+
+/*var idade = 18;
+var auto = true;
+
+if(idade >= 18 || auto == true){
+    console.log("Pode ir para balada")
+} else{
+    console.log("Não pode ir para a balada")
+}*/
+
+/*var peso = 45;
+var dieta = true;
+var nome = "Ranyeri";
+
+if(peso >= 45 && nome == "Ranyeri" || dieta == true){
+    console.log("tem que fazer dieta")
+} else{
+    console.log("Não precisa fazer dieta")
+};*/
+
+document.write("<p>O mais indicado é isolar as operações caso tenha mais operadores envouvidos</p>")
+
+
+
