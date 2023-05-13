be1e2417c2f3d05c57330bd566b9db220ed5df9e
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-13 18:41:23 -0300

    Loop em for
    Estrutura de repetição em for

---
+++ 20_EstRepeti_FOR/script.js
@@ -0,0 +1,18 @@
+document.write("O for é uma forma de estrutura de repetição mais enchuta e mais utlizada, devido que colocamos todas as instruções em uma só linha, invez do while que colocamos estruções em cascata");
+document.write("<p>A logica basica da intrução é: qual o valor, até quando você que que 'conte' o valor e de quanto em quanto </p>")
+
+for(var i = 0; i<=11; i++){
+   console.log("O numero é:" + i);
+}
+
+var r = "Ranyeri";
+
+for(var n = 0; n < 7; n++){
+   console.log(r[n]);
+}
+
+var arr = ["Ranyeri", "Karen", "Safira","Ágatha", "Nathalie"]
+
+for(var f = 0; f <= 4; f++){
+   console.log(arr[f])
+};
