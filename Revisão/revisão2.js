9ed5fd2aae90def03694736dbe2f4376d2f1d373
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-14 20:05:26 -0300

    If, else, if else
    Revisão de operadores de condição

--- Revisão/revisão2.js
+++ Revisão/revisão2.js
@@ -6,7 +6,7 @@ console.log(typeof 30)// number
 document.write("<h2>Variaveis</h2><p>Variais são utilizados para registrar o valor para posteriormente ser incluso no programa</p>")
 
 var x = 5;// variavel 'x' declarada
-var y = 2; // variavel 'y' declarada
+var y = 4; // variavel 'y' declarada
 console.log(x + y);
 
 document.write("<h2>Boolean</h2><p>Boolean é a conceituamente inspirando na equação de boolean que é true e false, quando a expressão der verdadeira é true e quando é falso é false</p>");
@@ -45,8 +45,52 @@ console.log(obj);
 console.log(obj.nome);// para acessar um dados especifico é colocar a varivial.o nome do dado selecionado
 console.log(typeof obj);
 
+document.write("<h2>Operadores de condição: if, else if e else</h2>")
 
+if(x > y){
+    console.log("O valor da primeira variavel é maior que da segunda");// se x for mais que y ira mostrar esse resultado
+} else if(x==y){
+    console.log("As variaveis são iguais"); // caso x não for maior que y e sim igual mostrara esse resultado
+}else{
+    console.log("As duas opção não são viaveis") // caso nenhuma das outras opções não for atendidas mostrará essa
+}
 
+document.write("<h2>Comparativo de dados: === e !==</h2><p>O === ele compara se dois dados são iguais, se são ate string ou numero e o !== verifica se é diferente</p>")
+
+var numero = 15;
+var letra = "15";
+
+if(numero == letra){
+    console.log("Se for igual ira aparecer pq esta utilizando o ==")// com o == o string e o number será dito como dados iguais
+}
+if(numero === letra){
+    console.log("Só é igual se os dois for string com string e number number")
+}
+
+document.write("<h2>Operadores logiscos: AND (&&), OR(||) e NOT(!)</h2>")
+
+let nun1 = 15;
+let nun2 = 26;
+let nun3 = 15;
+let nun4 = 30;
+
+document.write("<ul><li>AND: Compara duas expressões, se as duas der verdadeira será acionado a função </li></ul>")
+
+if(nun1 == nun3 && (nun1+nun3==nun4)){
+    console.log("Duas expressões verdadeira, com isso sera dada como verdadeira a expressão");
+};
+
+document.write("<ul><li>OR: Compara uma duas expressõe der verdadeira a expressão será verdadeira </li></ul>")
+
+if(nun1 < nun2 || nun2 > nun4){
+    console.log("Uma expressão falsa e uma verdadeira, com o OR a expressão dara como verdadeira");
+};
+
+document.write("<ul><li>NOT: Inverte o resultador da expressão </li></ul>")
+
+if(!(false)){
+    console.log("Inverte o valor")
+};
 
 
 
