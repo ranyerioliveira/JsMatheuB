4b0e06af6a402033373393b588233a4ad0f6d290
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-16 22:18:31 -0300

    Function
    function

--- 21_break_e_continue/script.js
+++ 21_break_e_continue/script.js
@@ -1,3 +1,5 @@
+document.write("<h1>Estudo de Break e Continue</h1><p>O Break é para parar serve para parar o loop no for ou no while</p><p>O Continue serve para pular o um valor especifico no loop</p>")
+
 for(var i = 10; i > 0;i-- ){
 
    console.log(i);
---
+++ 22_function/script.js
@@ -0,0 +1,2 @@
+document.write("<h1>Functions</h1><p>São blocos de codigos que pode ser invocados e reutilizados a qualquer momento no codigo, com isso economizando no tamanho da estrutura do codigo e na manutenção do proprio.</p>")
+
