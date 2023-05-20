52db325a34bd1bf42a658a947a731974289bcf6c
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-20 19:38:03 -0300

    For na pratica
    Praticando

---
+++ 23_for_na_pratica/script.js
@@ -0,0 +1,11 @@
+document.write("<h1>For na pratica</h1><br><p>Utilizando o que foi estudado até agora na prática.</p>")
+
+var lista = ["Laranja", "Maçã", "Pera", "Jaca", "Limão"]
+
+//inserir no HTML
+
+var listaUl = document.createElement("ul");
+
+var body = document.getElementsByTagName("body");
+
+console.log(body)
