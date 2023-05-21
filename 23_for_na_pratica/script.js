da006ac2d1e81aa94b6be4a97fb8ad3831ae255d
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-21 08:22:47 -0300

    Pratica de for
    For

--- 23_for_na_pratica/script.js
+++ 23_for_na_pratica/script.js
@@ -1,6 +1,6 @@
 document.write("<h1>For na pratica</h1><br><p>Utilizando o que foi estudado até agora na prática.</p>")
 
-var lista = ["Laranja", "Maçã", "Pera", "Jaca", "Limão"]
+var lista = ["Laranja", "Maçã", "Pera", "Jaca", "Limão","Ranyeri"]
 
 //inserir no HTML
 
@@ -8,4 +8,24 @@ var listaUl = document.createElement("ul");
 
 var body = document.getElementsByTagName("body");
 
-console.log(body)
+//console.log(body[0]);
+
+// incluir a lista no html no body
+
+body[0].appendChild(listaUl);
+
+var listaNoBody = document.getElementsByTagName("ul");
+
+console.log(listaNoBody[0]);
+
+for (var i = 0; i < lista.length; i++){
+
+    var liFor = document.createElement("li");
+
+    var textoLi = document.createTextNode(lista[i]);
+
+    liFor.appendChild(textoLi);
+
+    listaNoBody[0].appendChild(liFor);
+}
+
