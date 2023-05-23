9f42f0b9918bbd305a22252fd18d5fd7586086eb
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-22 21:11:39 -0300

    teste dom
    Testando DOM

---
+++ 23_for_na_pratica/teste.js
@@ -0,0 +1,15 @@
+var titulo = document.querySelector("#dom");
+
+console.log(titulo);
+console.log(titulo.textContent);
+
+titulo.textContent = "Texto modificado com textContent"
+
+var conteudo = document.querySelector(".mani");
+
+console.log(conteudo);
+console.log(conteudo.textContent);
+
+conteudo.textContent = "Com o textContent você consegue manipular o conteudo de texto do html"
+
+console.log(conteudo.textContent);
