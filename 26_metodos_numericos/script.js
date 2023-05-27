4f6d2569489a6c4d422327e12dd1bc96bfa756be
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-27 20:22:19 -0300

    metod nunber
    parseInt, parseFloat

---
+++ 26_metodos_numericos/script.js
@@ -0,0 +1,14 @@
+document.write("<h1>Metodos Numericos - parseInt, parseFloat</h1><p></p>")
+
+var x = parseFloat("25.587");
+
+console.log(typeof x)
+
+console.log(x)
+
+var y = parseInt("11")
+
+console.log(typeof y)
+
+console.log(y)
+
