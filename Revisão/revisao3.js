73cfa7902ff400d12a71895a259e79ed7f201ba1
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-29 19:24:38 -0300

    revisão function
    function

--- Revisão/revisao3.js
+++ Revisão/revisao3.js
@@ -1,11 +1,11 @@
-document.write("<h1>Revisão 3</h1><h2>Função</h2><p>Function: é uma forma de padronizar uma função especifica e utilizar ela quando quiser no codigo</p>")
+document.write("<h1>Revisão 3</h1><h2>Função</h2><p>Function: é uma forma de padronizar uma função especifica e utilizar ela quando quiser no codigo.</p>")
 
 function nome(){
     var x = "Ranyeri";
     document.write(x)
 }
 
-nome();
+//nome();
 
 function teste(){
     var n = "Safira";
@@ -13,4 +13,20 @@ function teste(){
     conteudo.textContent = n;
 }
 
-teste();
+//teste();
+
+document.write("<h3>Parametro</h3><p>Da pra colocar parametros na função que pode ser utilizada ao decorrer do codigo, como por exemplo uma soma, é só colocar variaveis para alocar o dado.</p>")
+
+function parametro(a, b){
+    var parametro = a + b;
+    return parametro;
+}
+
+var soma = parametro("Ranyeri ", "Oliveira");
+
+console.log(soma)
+
+var soma2 = parametro(20, 10);
+
+console.log(soma2);
+
