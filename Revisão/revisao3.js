2a7e28694b462698ca227eaabc42b760bf594819
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-29 20:13:59 -0300

    revisão 3
    Break e continue
    let e const

--- Revisão/revisao3.js
+++ Revisão/revisao3.js
@@ -30,3 +30,29 @@ var soma2 = parametro(20, 10);
 
 console.log(soma2);
 
+for(j = 10; j > 0; j-- ){
+    console.log(j)
+    if(j === 4){
+        break
+    }
+}
+
+document.write("<h2>let e const</h2><p>let: registra o dado igual ao var, podendo ser modificado o dados no codigo quando quiser</p><p>const: registra o dado, porem não pode ser modificado e o dados será sempre constante</p>")
+
+let dado1 = 5;
+
+console.log(dado1);
+
+dado1 = 6;
+
+console.log(dado1); // o dados modificado
+
+const dado2 = 25;
+
+console.log(dado2);
+
+dado2 = 30;
+
+console.log(dado2)// o dado não pode ser modificado
+
+
