c6faaa530c549e79b67f29c267c7ae8c5ab31096
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-28 21:23:54 -0300

    parseint, parsefloat e tofixed
    parseint, parsefloat e tofixed

--- 26_metodos_numericos/script.js
+++ 26_metodos_numericos/script.js
@@ -1,14 +1,18 @@
 document.write("<h1>Metodos Numericos - parseInt, parseFloat</h1><p></p>")
 
-var x = parseFloat("25.587");
+var x = parseFloat("25.587");// passa para o numero real, pode ser nunber ou string
 
 console.log(typeof x)
 
 console.log(x)
 
-var y = parseInt("11")
+var y = parseInt("11.5") //passa sempre para o numero inteiro, pode ser nunber ou string
 
 console.log(typeof y)
 
 console.log(y)
+
+var z = 24.548414
+
+console.log(z.toFixed(2)) // conta a quantidade de casa decimais
 
