da8bc90e2d855320fe59748d891c22c75dd88703
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-05-31 21:40:55 -0300

    metodos de strings
    Metodos de strings

---
+++ 27_metodos_strings/script.js
@@ -0,0 +1,35 @@
+document.write("<h1>Metodos Strings</h1><p>length: conta a quantidades de caractereris tem na string </p><p>indexOf: Mosta pela posição qual é o caracteris, utiliza p [], ou tambem pode verificar quando começa uma frase var frase = 'Eu vou ser um programador 'console.log(frase.indexOf('ser'))</p><p>slice: Extrair uma palavra da frase</p><p>replace: Serve para substituir uma frase na frase</p>")
+
+
+//length
+var nome = "Agatha";
+
+
+console.log(nome.length);
+
+//indexOf
+
+var nome2 = "Safira";
+
+console.log(nome2[2]);
+
+var frase = "Eu vou ser um programador"
+
+console.log(frase.indexOf("ser"))
+
+//slice
+
+var ser = frase.slice(7, 11);
+
+console.log(ser)
+
+//replace
+
+var novaPalavra = frase.replace("programador", "pai");
+
+console.log(novaPalavra);
+
+
+
+
+
