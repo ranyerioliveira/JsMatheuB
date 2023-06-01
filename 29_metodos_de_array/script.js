9b4b371b20d5d5ecaaeeed0366cb38e8f4b43c0e
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-01 20:43:14 -0300

    Metdos de arrays
    Push e length

---
+++ 29_metodos_de_array/script.js
@@ -0,0 +1,17 @@
+document.write("<h1>Metodos de arrays</h1><p>length: Contagem de itens dentro de uma array</p><p>push: Incluir um ou mais item dentro de uma array</p>")
+
+//length
+
+var contagem = [1, 2, 3, "Ranyeri", 5]
+
+console.log(contagem);// array
+
+console.log(contagem.length); //quantidade de itens
+
+//push
+
+contagem.push("Safira");
+
+console.log(contagem)
+
+
