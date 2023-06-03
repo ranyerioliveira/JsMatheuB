3728befbf068afe0cf02a6ae9b10e9ca98e6d4d8
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-03 17:48:09 -0300

    this
    Não faz sentido colocar essa porra do this em tudo

--- 32_this_instrução/script.js
+++ 32_this_instrução/script.js
@@ -1,5 +1,23 @@
 document.write("<h1>This</h1><p></ p>")
 
+var teste = 5;
+
+console.log(this.teste);
 
 
+
+//console.log(this);
+
+//alert("ola");
+//this.alert("ola 2");
+
+let pessoa = {
+    nome: "Ranyeri",
+    idade: 34,
+    peso: 80,
+    frase: function(){
+        console.log("Essa é uma função dentro de um objeto");
+    },
+    
+};
 
