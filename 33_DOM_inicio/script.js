039a39b6d0421bc52e95d59d5cc5a383d9ffab2f
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-05 21:56:48 -0300

    Continue
    cONTINIAÇÃO DE dom

--- 33_DOM_inicio/script.js
+++ 33_DOM_inicio/script.js
@@ -1,2 +1,2 @@
-document.write("<h1>DOM</h1><p></p>")
+document.write("<h1>DOM</h1><p>O DOM nada mais é que modificação do HTML via javascript por meio de metodos, podendo manipular eventos.</p><p>O DOM é uma copia do HTML e não ele em si.</p><p>A arvore DOM serve para saber onde será feira a mudança no HTML:</p><p><img src='Capturar.PNG' alt='' height='400px'></p>")
 
