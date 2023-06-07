62b05f61313bf0f3216a7d34e1bc48573e7bef62
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-06 23:46:29 -0300

    queryselectorAll
    queryselector

---
+++ 34_querySelector/script.js
@@ -0,0 +1,15 @@
+//queryselector
+
+var itensClasss = document.getElementsByClassName("item");
+
+console.log(itensClasss);
+
+var itensQuery = document.querySelectorAll("#lista2 li");
+
+console.log(itensQuery);
+
+//var teste = document.getElementsByClassName("teste");
+
+//console.log(teste);
+
+var itensQuery2 = document.querySelectorAll("#lista2 .itens")
