07e4c3e3a73a4416f882cebfd8219de45c61ba8c
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-13 18:28:26 -0300

    Continuando
    Continunando removendo conteudo do html

--- 37_remover_elementos/script.js
+++ 37_remover_elementos/script.js
@@ -1,1 +1,13 @@
+//removendo elementos filho
 
+var container = document.querySelector("#container")
+
+var p = document.querySelector("#container p");
+
+container.removeChild(p);
+
+// remover o elemento em si
+
+var subtitle = document.querySelector("#subtitle");
+
+subtitle.remove();
