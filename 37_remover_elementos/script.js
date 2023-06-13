a686ac8df37a08554503af78a9d1e6fe7183767b
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-13 19:03:20 -0300

    Remover elementos
    Remover elementos

--- 37_remover_elementos/script.js
+++ 37_remover_elementos/script.js
@@ -4,7 +4,10 @@ var container = document.querySelector("#container")
 
 var p = document.querySelector("#container p");
 
+//var h3 = document.querySelector("#container h3")
+
 container.removeChild(p);
+//container.removeChild(h3)
 
 // remover o elemento em si
 
