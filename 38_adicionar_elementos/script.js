5870a3dca4caee807f611f7109706ad195f4c632
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-13 20:03:30 -0300

    finalizando
    Finalizando inserir elementos

--- 38_adicionar_elementos/script.js
+++ 38_adicionar_elementos/script.js
@@ -14,6 +14,19 @@ var container = document.querySelector("#container")
 
 container.appendChild(el);
 
+//inserBefore - inserir antes
+
+var el2 = document.createElement("div");
+
+el2.classList = "div-before";
+
+var el3 = document.querySelector("#container .div-before");
+
+console.log(el3)
+
+container.insertBefore(el2, el3)
+
+
 
 
 
