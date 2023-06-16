b85c8f11c2af33ee26cc2d5c9277ac15de0539c7
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-16 10:52:36 -0300

    Atributos
    Alterando atributos

--- 40_alterando_atributos/script.js
+++ 40_alterando_atributos/script.js
@@ -1,8 +1,15 @@
+var title = document.querySelector("#title")
 
+title.setAttribute("class", "testando-atributo");// colocando um class na tag com o serAttribute
 
+console.log(title);
 
+var btn = document.querySelector("#btn");
 
+btn.setAttribute("disabled", "disabled"); //exemplo de desbilitar um botão
 
+var cor = document.querySelector("#cor")
 
+cor.setAttribute("color", "red");
 
 
