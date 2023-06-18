14caa4a6cc36d6ffc22446b5d8721c22fcf45076
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-17 22:23:28 -0300

    Modificando CSS
    Modificando css com javascript

---
+++ 41_alterando_css/script.js
@@ -0,0 +1,15 @@
+//slecionando elemento que será modificado com o css
+
+var title = document.querySelector("#title");
+
+//adicionar o estilo
+
+title.style.color = "blue" // varivel, css e o atributo.
+
+
+var corpo = document.querySelector("body")
+
+corpo.style.background = "red";
+
+
+
