130be5b8d488ef07999ad9dab1a3595afd27828f
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-17 22:29:36 -0300

    css
    css

--- 41_alterando_css/script.js
+++ 41_alterando_css/script.js
@@ -6,10 +6,15 @@ var title = document.querySelector("#title");
 
 title.style.color = "blue" // varivel, css e o atributo.
 
+title.style.background = "yellow"
 
 var corpo = document.querySelector("body")
 
 corpo.style.background = "red";
 
 
+// adicionar varios estilos de CSS
 
+var frase = document.querySelector("#cor");
+
+frase.style.cssText = "color: white; background: blue; font-size: 40px; text-align:center;";
