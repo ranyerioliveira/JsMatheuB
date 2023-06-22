6ab8bb9ef217ba94095844939730848a6fba6e64
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-22 19:55:56 -0300

    click
    click

--- 47_evento_click/script.js
+++ 47_evento_click/script.js
@@ -1,1 +1,15 @@
 //evento de click
+
+var btn = document.querySelector("#btn")
+
+console.log(btn);
+
+btn.addEventListener("click", function(){
+
+console.log("Clicou")
+//console.log(this);
+this.style.color = "red"// consegue modificar a propriedade utlizando a instrução "this"
+
+}
+
+);// adicionar um evento de click
