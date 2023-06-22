217c75b402da9c07c182eac2ea236475dfacc8ee
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-22 19:22:39 -0300

    constinuando
    continuação

--- 45_clearTimeout_clearInterval/script.js
+++ 45_clearTimeout_clearInterval/script.js
@@ -1,1 +1,33 @@
 // clearTimeout e clearInterval
+
+
+//clearTimeout
+
+var x = 0;
+
+var myTime = setTimeout(function(){
+    console.log("O x é igual a 0")
+},1500);
+
+x = 5;
+
+if(x > 0){
+    clearTimeout(myTime);
+    console.log("o X passou de 0")
+}
+
+//clearInterval
+
+var myInterval = setInterval(function(){
+
+    console.log("Imprimindo intervalo");
+
+},500);
+
+setTimeout(function(){
+
+    console.log("Não precisa mais repetir");
+    clearInterval(myInterval);
+
+},4500);
+
