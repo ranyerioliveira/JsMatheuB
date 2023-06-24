641291caaaf948eea6941d4629fd9e3b157060f1
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-24 17:29:07 -0300

    eventos de mouse
    Mouseover e mouse out

---
+++ 48_evento_mouseover_mouseout/script.js
@@ -0,0 +1,24 @@
+//evento de mouseover e mouseout
+
+//mouseover
+
+var title = document.querySelector("#title");
+
+title.addEventListener("mouseover", function(){
+
+    this.style.backgroundColor = "red";
+}
+
+
+);
+
+//mouseout
+
+title.addEventListener("mouseout", function(){
+
+    this.style.backgroundColor = " rgb(234, 0, 255)"
+
+}
+);
+
+//basicamento o codico assima reproduz um hover de css
