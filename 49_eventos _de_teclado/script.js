e842bd5ab9383da6b23c59cf68ae58979986a119
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-25 13:15:59 -0300

    eventos de teclado
    keydown e keyup

--- 49_eventos _de_teclado/script.js
+++ 49_eventos _de_teclado/script.js
@@ -1,47 +1,21 @@
-//evento de mouseover e mouseout
+//eventos de teclado keydown keyup 
 
-//mouseover
+document.addEventListener("keydown", function(event){
 
-var title = document.querySelector("#title");
+    console.log(event.key)
 
-title.addEventListener("mouseover", function(){
+});
 
-    this.style.backgroundColor = "red";
-}
+//keyup
 
+document.addEventListener("keyup", function(e){
+    if(event.key === "Enter"){
+        console.log("Soltou o enter")
+    }
+})
 
-);
-
-//mouseout
-
-title.addEventListener("mouseout", function(){
-
-    this.style.backgroundColor = " rgb(234, 0, 255)"
-
-}
-);
-
-//basicamento o codico assima reproduz um hover de css
-
-
-//afetar outro elemento com mouseover
-//quando passar pelo subtitulo o texto vai sumir
-
-var subtitulo = document.querySelector("#subtitulo") //seleciona o subtitulo que o mouse vai passar
-
-//agora é criar o enveto
-subtitulo.addEventListener("mouseover", function(){
-
-    var legenda = document.querySelector("#legenda");//adiciona a legenda que foi criada com id e class
-    legenda.classList.remove("hide") //cria o metodo para remover a class
-}
-
-)
-// para aparece novamento
-subtitulo.addEventListener("mouseout", function(){
-
-    var legenda = document.querySelector("#legenda");//adiciona a legenda que foi criada com id e class
-    legenda.classList.add("hide") //cria o metodo para adicionar a class
-}
-
-)
+document.addEventListener("keyup", function(f){
+    if(event.key === "f"){
+        console.log("Soltou o f")
+    }
+})
