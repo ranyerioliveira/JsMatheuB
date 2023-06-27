bcd71045765b3baed81093a6fc85f2de4938f260
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-06-27 17:24:33 -0300

    Todo list
    Colocando o JS

--- 50_projeto/tolist.js
+++ 50_projeto/tolist.js
@@ -1,2 +1,22 @@
 //adicionar a tarefa
 
+//função de adicionar tarefa
+
+function addTask(){
+
+    //titulo da tarefa
+    const taskTitle = document.querySelector("#task-title").value;//o value serve para pega o valor do input
+    console.log(taskTitle)
+}
+
+//evento de adicionar tarefa
+
+const addBtn = document.querySelector("#add-bnt");
+
+addBtn.addEventListener("click", function(e){// adicionar evento ao botão
+
+    e.preventDefault(); //não submeter ao formulario, quando aperta o btão ele não vai 
+
+    addTask()
+
+})
